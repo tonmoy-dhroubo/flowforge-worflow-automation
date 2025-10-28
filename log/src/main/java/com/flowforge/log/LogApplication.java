ae46ae838a0f39819b1319b54f31abd5a7bf357b
Tonmoy Ahmed Dhroubo <tonmoyahmeddhroubo@gmail.com>
2025-10-29 02:03:42 +0600

    fix public for main methods & remove groupId for child projects

--- log/src/main/java/com/flowforge/log/LogApplication.java
+++ log/src/main/java/com/flowforge/log/LogApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LogApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(LogApplication.class, args);
 	}
 
--- trigger/src/main/java/com/flowforge/trigger/TriggerApplication.java
+++ trigger/src/main/java/com/flowforge/trigger/TriggerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TriggerApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(TriggerApplication.class, args);
 	}
 
